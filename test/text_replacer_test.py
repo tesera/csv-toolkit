63a508b6ae85ce458435ca5b852b92dc759b6067
zmpeg <zandstra.matt@gmail.com>
2016-04-03 21:57:57 -0600

    moved replace_test into text_replacer_test

--- test/text_replacer_test.py
+++ test/text_replacer_test.py
@@ -1,4 +1,5 @@
 import unittest
+from StringIO import StringIO
 from replace import TextReplacer
 
 class ParserTest(unittest.TestCase):
@@ -21,3 +22,27 @@ class ParserTest(unittest.TestCase):
     def test_input(self):
         replacer = TextReplacer(['--search', '""', '--replace', '""', '--field', '""', '--input', self.expected_file])
         self.assertEqual(replacer.args.input.name, self.expected_file)
+
+class ReplacerTest(unittest.TestCase):
+    def setUp(self):
+        self.sample_csv = 'test/fixtures/sample-dirty.csv'
+
+    def file_contents(self, csv):
+        with open(csv) as f:
+            return f.read()
+
+    def test_global_replace(self):
+        expected_csv = 'test/fixtures/sample-clean.csv'
+        replacer = TextReplacer(['--search', 'NA', '--replace', '', '--input', self.sample_csv])
+        replacer.args.output = StringIO()
+        replacer.replace()
+        self.assertEqual(replacer.args.input.name, self.sample_csv)
+        self.assertEqual(replacer.args.output.getvalue(), self.file_contents(expected_csv))
+
+    def test_field_replace(self):
+        expected_csv = 'test/fixtures/sample-clean-name.csv'
+        replacer = TextReplacer(['--search', 'NA', '--replace', '', '--field', 'NAME', '--input', self.sample_csv])
+        replacer.args.output = StringIO()
+        replacer.replace()
+        self.assertEqual(replacer.args.input.name, self.sample_csv)
+        self.assertEqual(replacer.args.output.getvalue(), self.file_contents(expected_csv))
