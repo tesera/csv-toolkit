75db92cb92f9ee1e2af7e6384269aa57ee81c85e
zmpeg <zandstra.matt@gmail.com>
2017-02-13 11:34:19 -0700

    add a script to select certain columns from a csv file and create a new csv file.

---
+++ copy_columns.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import argparse
+import csv
+import sys
+import re
+flatten = lambda l: [item for sublist in l for item in sublist]
+
+def main():
+    parser = argparse.ArgumentParser('./copy_columns.py', description='Copy columns from one CSV into a new one.')
+    parser.add_argument('-f', '--filters', help='Prefix to filter csv files before join', required=True, action='append', nargs='*')
+    parser.add_argument('-i', '--input', type=argparse.FileType('r'), default=sys.stdin, help='Input file', required=False)
+    parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=sys.stdout, help='Output file', required=False)
+    args = parser.parse_args()
+    filters = None
+
+    for index, row in enumerate(csv.reader(iter(args.input.readline, ''), delimiter=',')):
+        writer = csv.writer(args.output, lineterminator="\n")
+        if index == 0:
+            filters = list(map(lambda r: filter(re.compile(r).match, row), flatten(args.filters)))
+            filters = flatten(filters)
+            filters = reduce(lambda x,y: x+[y] if not y in x else x, filters,[])
+            filters = map(lambda col: row.index(col), filters)
+            filters = sorted(filters)
+        else:
+            row = [c for index, c in enumerate(row) if index in filters]
+            writer.writerow(row)
+
+
+if __name__ == "__main__":
+    main()
